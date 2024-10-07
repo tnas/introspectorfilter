dccaabe43496d04d88fbd8a8baedbc5296e5f6e9
Thiago Nascimento <nascimenthiago@gmail.com>
2024-10-07 16:49:14 -0300

    Adding suite for tests

--- src/main/java/com/dzone/tnas/introspectorfilter/annotation/Filterable.java
+++ src/main/java/com/dzone/tnas/introspectorfilter/annotation/Filterable.java
@@ -1,6 +1,5 @@
 package com.dzone.tnas.introspectorfilter.annotation;
 
-import java.lang.annotation.Annotation;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
---
+++ src/test/java/com/dzone/tnas/introspectorfilter/IntrospectorFilterTestSuite.java
@@ -0,0 +1,10 @@
+package com.dzone.tnas.introspectorfilter;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages({ "com.dzone.tnas.introspectorfilter" })
+public class IntrospectorFilterTestSuite {
+
+}
