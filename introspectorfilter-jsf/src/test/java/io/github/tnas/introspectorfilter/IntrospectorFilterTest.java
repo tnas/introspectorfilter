105c3abbca8b0623673e80b433a1701e2e084197
Thiago Nascimento <nascimenthiago@gmail.com>
2024-10-23 16:46:22 -0300

    Refactor pom of parent and submodules and improve tests

--- introspectorfilter-jsf/src/test/java/io/github/tnas/introspectorfilter/IntrospectorFilterTest.java
+++ introspectorfilter-jsf/src/test/java/io/github/tnas/introspectorfilter/IntrospectorFilterTest.java
@@ -4,8 +4,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Random;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -25,34 +27,82 @@ class IntrospectorFilterTest {
 
     @Test
     void shouldFilterByTextInPublications() {
-    	var filteredList = postsCollection.stream().filter(p -> filter.filter(p, "vitae")).toList();
-        assertEquals(2, filteredList.size());
+
+        var textFilter = "vitae";
+
+        var traditionalFilteredList = postsCollection.stream()
+                .filter(p -> Objects.isNull(p.getText()) ||
+                        StringUtils.stripAccents(p.getText().trim().toLowerCase()).contains(textFilter.trim().toLowerCase()))
+                .toList();
+
+    	var filteredList = postsCollection.stream().filter(p -> filter.filter(p, textFilter)).toList();
+        assertEquals(traditionalFilteredList.size(), filteredList.size());
         assertEquals(1, filteredList.getFirst().getId());
         assertEquals(6, filteredList.getLast().getId());
+        assertEquals(1, traditionalFilteredList.getFirst().getId());
+        assertEquals(6, traditionalFilteredList.getLast().getId());
     }
     
     @Test
     void shouldFilterByHashtags() {
+
+        var textFilter = "HECUBA";
+
+        var traditionalFilteredList = postsCollection.stream()
+                .filter(p -> Objects.isNull(p.getHashtags()) || p.getHashtags().isEmpty() ||
+                        p.getHashtags().stream().anyMatch(h ->
+                                StringUtils.stripAccents(h.trim().toLowerCase())
+                                        .contains(textFilter.trim().toLowerCase())))
+                .toList();
+
     	var filteredList = postsCollection.stream().filter(p -> filter.filter(p, "Hecuba")).toList();
-        assertEquals(1, filteredList.size());
+        assertEquals(traditionalFilteredList.size(), filteredList.size());
         assertEquals(7, filteredList.getFirst().getId());
         assertEquals("Mapin Publishing", filteredList.getFirst().getComments().getFirst().getReview());
+
+        assertEquals(7, traditionalFilteredList.getFirst().getId());
+        assertEquals("Mapin Publishing", traditionalFilteredList.getFirst().getComments().getFirst().getReview());
     }
     
     @Test
     void shouldFilterByReviewInComments() {
+
+        var textFilter = "Harlequin";
+
+        var traditionalFilteredList = postsCollection.stream()
+                .filter(p -> Objects.isNull(p.getComments()) || p.getComments().isEmpty() ||
+                        p.getComments().stream().anyMatch(c ->
+                                StringUtils.stripAccents(c.getReview().trim().toLowerCase())
+                                        .contains(textFilter.trim().toLowerCase())))
+                .toList();
+
     	var filteredList = postsCollection.stream().filter(p -> filter.filter(p, "Harlequin")).toList();
-        assertEquals(1, filteredList.size());
+        assertEquals(traditionalFilteredList.size(), filteredList.size());
         assertEquals(3, filteredList.getFirst().getId());
-        assertEquals("United States Government Publishing Office", filteredList.getFirst().getComments().getFirst().getReview());
+        assertEquals("United States Government Publishing Office",
+                filteredList.getFirst().getComments().getFirst().getReview());
+
+        assertEquals(3, traditionalFilteredList.getFirst().getId());
+        assertEquals("United States Government Publishing Office",
+                traditionalFilteredList.getFirst().getComments().getFirst().getReview());
     }
     
     @Test
     void shouldFilterByIntegerRelevanceInPublications() {
+
+        var textFilter = 6;
+
+        var traditionalFilteredList = postsCollection.stream()
+                .filter(p -> p.getRelevance() == textFilter)
+                .toList();
+
     	var filteredList = postsCollection.stream().filter(p -> filter.filter(p, 6)).toList();
-        assertEquals(2, filteredList.size());
+        assertEquals(traditionalFilteredList.size(), filteredList.size());
         assertEquals(1, filteredList.getFirst().getId());
         assertEquals(4, filteredList.getLast().getId());
+
+        assertEquals(1, traditionalFilteredList.getFirst().getId());
+        assertEquals(4, traditionalFilteredList.getLast().getId());
     }
     
     @Test
