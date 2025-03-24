c810db8333a0064d3677fcaea4dae9da6fed5365
Thiago Nascimento <nascimenthiago@gmail.com>
2025-03-24 07:16:00 -0300

    test: Add results

--- introspectorfilter-core/test.sh
+++ introspectorfilter-core/test.sh
@@ -15,7 +15,7 @@ fi
 
 mvn install:install-file -Dfile=dj.jar -DgroupId=edu.neu.ccs.demeter -DartifactId=dj -Dversion=0.8.6 -Dpackaging=jar
 
-GRAPH_SIZE=300
+GRAPH_SIZE=100
 
 UNIT_TEST="DemeterDJTest#found_one_instance"
 mvn -DargLine="-DGRAPH_SIZE=$GRAPH_SIZE -DNUM_THREADS=1" -Dtest="$UNIT_TEST" test
